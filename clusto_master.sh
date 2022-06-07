57b3149ae22d4d4037b87ecfa4bf12baa5ba99e9
Jack-WessANU <84662674+Jack-WessANU@users.noreply.github.com>
2022-06-07 11:18:08 +1000

    Update clusto_master.sh

--- clusto_master.sh
+++ clusto_master.sh
@@ -8,11 +8,11 @@ cp input_data/Penicillium_sp._X.gff3 above_4
 
 cd above_4
 
-## Convert our csv file to a tab-delimited one. 
+## Convert our csv file to a tab-delimited one
 
 sed 's/\,/\t/g' inplantavsinvitro.csv > inplantavsinvitro.txt
 
-## Write an if loop that if the LFC (column 2) is above a specified value, then pull the ID (column 1) to a new txt file for grepping against gff3 file. 
+## Write an if loop that if the LFC (column 2) is above a specified value, then pull the ID (column 1) to a new txt file for grepping against gff3 file 
 
 while read LINE; do
 
@@ -26,7 +26,7 @@ if ($2 >= minimumLFC)
 
 done<inplantavsinvitro.txt
 
-## Use this new selective gene id file to modify a gff3 to only contain genes regulated above or below the specified spot. 
+## Use this new selective gene id file to modify a gff3 to only contain genes regulated above or below the specified spot
 
 grep -f selective_geneids.txt Penicillium_sp._X.gff3 > Penicillium_sp._X_above_four.gff3.txt
 
@@ -35,12 +35,12 @@ grep -f selective_geneids.txt Penicillium_sp._X.gff3 > Penicillium_sp._X_above_f
 grep "mRNA" Penicillium_sp._X_above_four.gff3.txt > Penicillium_sp._X_above_four_onlymrna.gff3.txt
 
 ## This step will make sure that the files contain all of the functional annotation, before hand it would cut off between hypothetical and protein as there was a space!
-## If the text is too unwieldy on the page, simply delete this line of code, it won't have an effect on the rest of the script. 
+## If the text is too unwieldy on the page, simply delete this line of code, it won't have an effect on the rest of the script
 
  awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9$10$11$12$13$14$15$16$17$18$19$20 }' Penicillium_sp._X_above_four_onlymrna.gff3.txt > Penicillium_sp._X_above_four_onlymrnacomb.gff3.txt
 
 
-## Then we need to separate them into contigs. 
+## Then we need to separate them into contigs
 
 mkdir passed_to_awk
 
@@ -54,7 +54,7 @@ do
 
 done
 
-## Step X. Modify gff3 to remove un-needed data and calculate mid-point of gene. 
+## Step X. Modify gff3 to remove un-needed data and calculate mid-point of gene
 
 for FILE in passed_to_awk/*;
 
@@ -64,7 +64,7 @@ do
 
 done 
 
-## The final selected-gene-location files will be in the passed_to_awk directory for use. 
+## The final selected-gene-location files will be in the passed_to_awk directory for use 
 
 cd passed_to_awk
 
@@ -74,7 +74,7 @@ python3 ~/inspector_clusto/inspector_package/scripts/clusto_window_looping.py > 
 
 cd ..
 
-## Now we need to separate the cluster mega into contig-clusters. 
+## Now we need to separate the cluster mega into contig-clusters
 ## Make a new directory for the new files
 
 mkdir separated_clusters
@@ -88,16 +88,11 @@ done
 
 cd separated_clusters
 
-## I then add '.txt' to the end of each file as I had to remove it before. 
+## I then add '.txt' to the end of each file as I had to remove it before
 
 find . -type f -exec bash -c 'mv "$0" "$0.txt"' {} \;
 
-python3 ~/inspector_clusto/inspector_package/scripts/clusto_grab.py > jim.txt
+## Then call the fucking done with it script, which will grep the windows that contain genes and print 
+## the number of genes present within them, then they can be moved to excel
 
-
-## Then create a list of files to use for grepping the next file. 
-
-for i in {0..10000000..30000};
-do
-    echo $i >> cluster_starts.txt
-done
+bash ~/inspector_clusto/inspector_package/scripts/clusto_ifdwt.sh
