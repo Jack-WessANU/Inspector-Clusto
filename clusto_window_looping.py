cd62def02c0de80bc029ba7d05c45f9b921f72d1
Jack-WessANU <84662674+Jack-WessANU@users.noreply.github.com>
2022-07-14 18:40:16 +1000

    Update and rename window/clusto_window_looping.py to clusto_window_looping.py
    Just basic changes to account for moving other files around.

--- clusto_window_looping.py
+++ clusto_window_looping.py
@@ -1,7 +1,7 @@
 ## Python code to slide over a modified gff3 file and pull out gene names as they appear in the sliding window (of a given nucleotide size)
 import os
 
-path = '/home/jack.a/inspector_clusto/inspector_package/above_4/passed_to_awk'
+path = '/home/jack.a/inspector_clusto/inspector_package/above_4_below_negative_4/passed_to_awk'
 
 folder = os.fsencode(path)
 
@@ -11,6 +11,6 @@ for file in os.listdir(folder):
                 columns = line.split()
                 gene_loc = float(columns[1]) ## Create the element gene_loc, the location of each gene.
                 gene_id = columns[0] ## Create the element gene_id, the name and functional annoation of each gene.
-                for cstart in range(0, 10000000, 30000):
-                        if cstart < gene_loc < cstart+90000:
-                                print(file, gene_id, cstart, cstart+90000)
+                for cstart in range(0, 10000000, 15000):
+                        if cstart < gene_loc < cstart+20000:
+                                print(file, gene_id, cstart, cstart+20000)
