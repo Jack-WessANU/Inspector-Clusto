464fd4d6e0921010536db70ab319e92912a9640b
Jack-WessANU <84662674+Jack-WessANU@users.noreply.github.com>
2022-08-16 18:31:01 +1000

    Update and rename clusto_master.sh to clusto_master_windows.sh

--- clusto_master_windows.sh
+++ clusto_master_windows.sh
@@ -23,14 +23,28 @@ sed 's/\,/\t/g' penx_master_table_inplantavsinvitro.csv > penx_master_table_inpl
 
 echo "CUTTING DOWN GFF3 TO JUST IMPORTANT PARTS + DIVIDING GFF3 UP INTO RESPECTIVE CONTIGS"
 
+while read LINE; do
+
+awk '{ minimumLFC=4 ;
+if ($2 >= 4 || $2 <= -4)
+    print $1 > "selective_geneids.txt"
+}'
+
+done<inplantavsinvitro.txt
+
+## Use this new selective gene id file to modify a gff3 to only contain genes regulated above or below the specified spot
+
+grep -f selective_geneids.txt Penicillium_sp._X.gff3 > Penicillium_sp._X_above_four_below_negative_four.gff3.txt
+
 ## Then grep it down to only mRNA
 
-grep "mRNA" Penicillium_sp._X.gff3 > Penicillium_sp._X_above_four_below_negative_fouronlymrna.gff3.txt
+grep "mRNA" Penicillium_sp._X_above_four_below_negative_four.gff3.txt > Penicillium_sp._X_above_four_below_negative_fouronlymrna.gff3.txt
 
 ## This step will make sure that the files contain all of the functional annotation, before hand it would cut off between hypothetical and protein as there was a space!
 ## If the text is too unwieldy on the page, simply delete this line of code, it won't have an effect on the rest of the script
 
  awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9$10$11$12$13$14$15$16$17$18$19$20 }' Penicillium_sp._X_above_four_below_negative_fouronlymrna.gff3.txt > Penicillium_sp._X_above_four_below_negative_fouronlymrnacomb.gff3.txt
+
 
 ## Then we need to separate them into contigs
 
